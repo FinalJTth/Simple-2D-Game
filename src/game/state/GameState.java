5dde72ba9fccf1725ee5b7cccac48270f3086464
Teeradham <5batman400@gmail.com>
2019-04-18 22:55:02 +0700

    add world and world loader, add utils class
    - add Utils.parseInt, Utils.loadFileAsString

--- src/game/state/GameState.java
+++ src/game/state/GameState.java
@@ -5,14 +5,17 @@ import java.awt.Graphics2D;
 import game.engine.GameThread;
 import game.entity.creature.Player;
 import game.tile.Tile;
+import game.world.World;
 
 public class GameState extends State {
 
 	private Player player;
+	private World world;
 
 	public GameState(GameThread gameThread) {
 		super(gameThread);
 		player = new Player(gameThread, 100, 100);
+		world = new World("res/worlds/world1.txt");
 		System.out.println("GameState init");
 	}
 
@@ -24,8 +27,8 @@ public class GameState extends State {
 
 	@Override
 	public void render(Graphics2D g2d) {
+		world.render(g2d);
 		player.render(g2d);
-		Tile.tiles[0].render(g2d, 0, 0);
 	}
 
 	public Player getPlayer() {
---
+++ src/game/world/World.java
@@ -0,0 +1,55 @@
+package game.world;
+
+import java.awt.Graphics2D;
+
+import game.tile.Tile;
+import game.utils.Utils;
+
+public class World {
+
+	private int width, height;
+	private int playerSpawnX, playerSpawnY;
+	private int[][] tiles;
+
+	public World(String path) {
+		loadWorld(path);
+	}
+
+	public void update() {
+
+	}
+
+	public void render(Graphics2D g2d) {
+		for (int row = 0; row < width; row++) {
+			for (int col = 0; col < height; col++) {
+				getTile(row, col).render(g2d, row * Tile.TILE_WIDTH, col * Tile.TILE_HEIGHT);
+			}
+		}
+	}
+
+	public Tile getTile(int row, int col) {
+		Tile t = Tile.tiles[tiles[row][col]]; // get Tile from tile id
+		if (t == null) {
+			return Tile.grassTile; // return default tile
+		}
+		return t;
+	}
+
+	private void loadWorld(String path) {
+		String file = Utils.loadFileAsString(path);
+		String[] tmp = file.split("\\s+");
+		System.out.println(file);
+		width = Utils.parseInt(tmp[0]);
+		height = Utils.parseInt(tmp[1]);
+		playerSpawnX = Utils.parseInt(tmp[2]);
+		playerSpawnY = Utils.parseInt(tmp[3]);
+
+		tiles = new int[width][height];
+		for (int col = 0; col < height; col++) {
+			for (int row = 0; row < width; row++) {
+				tiles[row][col] = Utils.parseInt(tmp[row + col * width + 4]); // turn 1D array to 2D
+			}
+		}
+	}
+
+}
