0586c8c81c8be524a89b104a6aebb0026eb794b1
Teeradham <5batman400@gmail.com>
2019-04-28 18:27:59 +0700

    Add big blob minion assets , checkpoint before mess thing up

--- src/game/graphics/Assets.java
+++ src/game/graphics/Assets.java
@@ -16,7 +16,9 @@ public class Assets {
 	public static BufferedImage[] play_button, sound_ON_button, sound_OFF_button, exit_button;
 	public static BufferedImage play_button_normal, sound_ON_button_normal, sound_OFF_button_normal, exit_button_normal;
 	public static BufferedImage play_button_hover, sound_ON_button_hover, sound_OFF_button_hover, exit_button_hover;
-	public static BufferedImage play_button_onClick, sound_ON_button_onClick, sound_OFF_button_onClick, exit_button_onClick;
+	public static BufferedImage play_button_onClick, sound_ON_button_onClick, sound_OFF_button_onClick,
+			exit_button_onClick;
+	public static BufferedImage[] big_blob_idle, big_blob_walk, big_blob_attack;
 
 	private static int cnt = 0;
 
@@ -63,54 +65,71 @@ public class Assets {
 				if (cnt == 11)
 					break;
 				if (cnt < 6)
-					iceShardSpell_firing[cnt] = Utils.resize(iceShardSheet.crop(y * 70, x * 70, 70, 70), 1.5f) ;
+					iceShardSpell_firing[cnt] = Utils.resize(iceShardSheet.crop(y * 70, x * 70, 70, 70), 1.5f);
 				else if (cnt == 6)
 					iceShardSpell_bullet = Utils.resize(iceShardSheet.crop(y * 70, x * 70, 70, 70), 1.5f);
 				else {
 					iceShardSpell_hit[cnt - 7] = Utils.resize(iceShardSheet.crop(y * 70, x * 70, 70, 70), 1.5f);
 				}
-					
+
 				cnt++;
 			}
 		}
-		cnt = 0;
+
+		SpriteSheet bigBlobSheet = new SpriteSheet(
+				ImageLoader.loadImage("res/texture/minion/bigBlob/bigBlobMinionSprite.png"));
+		big_blob_idle = new BufferedImage[6];
+		big_blob_walk = new BufferedImage[8];
+		big_blob_attack = new BufferedImage[8];
+		for (int x = 0; x < 3; x++) {
+			cnt = 0;
+			for (int y = 0; y < 7; y++) {
+				if (x == 0 && y < 6) {
+					big_blob_idle[y] = bigBlobSheet.crop(y * 80, x * 80, 80, 80);
+				} else if (x == 1) {
+					big_blob_walk[y] = bigBlobSheet.crop(y * 80, x * 80, 80, 80);
+				} else {
+					big_blob_attack[y] = bigBlobSheet.crop(y * 80, x * 80, 80, 80);
+				}
+			}
+		}
 
 		SpriteSheet buttonSheet = new SpriteSheet(ImageLoader.loadImage("res/menuButton/buttonSheet.png"));
 		play_button_normal = buttonSheet.crop(0, 0, 40, 21);
 		exit_button_normal = buttonSheet.crop(41, 0, 40, 21);
-		sound_ON_button_normal = buttonSheet.crop(41*2, 0, 40, 21);
-		sound_OFF_button_normal = buttonSheet.crop(41*3, 0, 40, 21);
-		
+		sound_ON_button_normal = buttonSheet.crop(41 * 2, 0, 40, 21);
+		sound_OFF_button_normal = buttonSheet.crop(41 * 3, 0, 40, 21);
+
 		play_button_hover = buttonSheet.crop(0, 22, 40, 21);
 		exit_button_hover = buttonSheet.crop(41, 22, 40, 21);
-		sound_ON_button_hover = buttonSheet.crop(41*2, 22, 40, 21);
-		sound_OFF_button_hover = buttonSheet.crop(41*3, 22, 40, 21);
-		
+		sound_ON_button_hover = buttonSheet.crop(41 * 2, 22, 40, 21);
+		sound_OFF_button_hover = buttonSheet.crop(41 * 3, 22, 40, 21);
+
 		play_button_onClick = buttonSheet.crop(0, 43, 40, 21);
 		exit_button_onClick = buttonSheet.crop(41, 43, 40, 21);
-		sound_ON_button_onClick = buttonSheet.crop(41*2, 43, 40, 21);
-		sound_OFF_button_onClick = buttonSheet.crop(41*3, 43, 40, 21);
-		
+		sound_ON_button_onClick = buttonSheet.crop(41 * 2, 43, 40, 21);
+		sound_OFF_button_onClick = buttonSheet.crop(41 * 3, 43, 40, 21);
+
 		play_button = new BufferedImage[3];
 		play_button[0] = play_button_normal;
 		play_button[1] = play_button_hover;
 		play_button[2] = play_button_onClick;
-		
+
 		exit_button = new BufferedImage[3];
 		exit_button[0] = exit_button_normal;
 		exit_button[1] = exit_button_hover;
 		exit_button[2] = exit_button_onClick;
-		
+
 		sound_ON_button = new BufferedImage[3];
 		sound_ON_button[0] = sound_ON_button_normal;
 		sound_ON_button[1] = sound_ON_button_hover;
 		sound_ON_button[2] = sound_ON_button_onClick;
-		
+
 		sound_OFF_button = new BufferedImage[3];
 		sound_OFF_button[0] = sound_OFF_button_normal;
 		sound_OFF_button[1] = sound_OFF_button_hover;
 		sound_OFF_button[2] = sound_OFF_button_onClick;
-		
+
 		// TODO temporary code. Will be replaced by SpriteSheet later.
 		grass = ImageLoader.loadImage("res/texture/grass.png");
 		lava = ImageLoader.loadImage("res/texture/lava.png");
