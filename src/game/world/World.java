24a0f87301406576707415d797dc45a00839219b
Teeradham <5batman400@gmail.com>
2019-05-11 20:20:03 +0700

    Fix even more error

--- src/game/world/World.java
+++ src/game/world/World.java
@@ -1,13 +1,13 @@
 package game.world;
 
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 
 import game.engine.GameThread;
 import game.entity.EntityManager;
 import game.entity.creature.Player;
 import game.entity.creature.minion.spawner.MinionSpawner;
 import game.entity.statics.Tree;
-import game.tile.CliffTile;
 import game.tile.Tile;
 import game.utils.Utils;
 
@@ -22,10 +22,10 @@ public class World {
 	public World(GameThread gameThread, String path) {
 		this.gameThread = gameThread;
 		entityManager = new EntityManager(gameThread, new Player(gameThread, 100, 100));
-		entityManager.addEntity(new Tree(gameThread, 550, 200, 60, 75, true));
-
+		entityManager.addEntity(new Tree(gameThread, 550, 200, 60, 75));
+		
 		loadWorld(path);
-
+		
 		entityManager.getPlayer().setxPos(playerSpawnX);
 		entityManager.getPlayer().setyPos(playerSpawnY);
 	}
@@ -37,58 +37,30 @@ public class World {
 	public void render(Graphics2D g2d) {
 		// render only tiles that player can see (on camera)
 		int rowStart = (int) Math.max(0, gameThread.getGameCamera().getxOffset() / Tile.TILE_WIDTH);
-		int rowEnd = (int) Math.min(width,
-				(gameThread.getGameCamera().getxOffset() + gameThread.getGame().getWindow().getWidth())
-						/ Tile.TILE_WIDTH + 1);
+		int rowEnd = (int) Math.min(width, (gameThread.getGameCamera().getxOffset() + gameThread.getGame().getWindow().getWidth()) / Tile.TILE_WIDTH + 1);
 		int colStart = (int) Math.max(0, gameThread.getGameCamera().getyOffset() / Tile.TILE_HEIGHT);
-		int colEnd = (int) Math.min(height,
-				(gameThread.getGameCamera().getyOffset() + gameThread.getGame().getWindow().getHeight())
-						/ Tile.TILE_HEIGHT + 1);
+		int colEnd = (int) Math.min(height, (gameThread.getGameCamera().getyOffset() + gameThread.getGame().getWindow().getHeight()) / Tile.TILE_HEIGHT + 1);
+
 		for (int row = rowStart; row < rowEnd; row++) {
 			for (int col = colStart; col < colEnd; col++) {
-				if (tiles[row][col] < 256) {
-					getTile(row, col).render(g2d,
-							(int) (row * Tile.TILE_WIDTH - gameThread.getGameCamera().getxOffset()),
-							(int) (col * Tile.TILE_HEIGHT - gameThread.getGameCamera().getyOffset()),
-							randomedTiles[row][col]);
-				} else if (tiles[row][col] > 255) {
-					if (getCliffTile(row, col).isCorner() == true && tiles[row][col] < 277) {
-						Tile.dirtTiles.render(g2d,
-								(int) (row * Tile.TILE_WIDTH - gameThread.getGameCamera().getxOffset()),
-								(int) (col * Tile.TILE_HEIGHT - gameThread.getGameCamera().getyOffset()),
-								randomedTiles[row][col]);
-					}
-					getCliffTile(row, col).render(g2d,
-							(int) (row * Tile.TILE_WIDTH - gameThread.getGameCamera().getxOffset()),
-							(int) (col * Tile.TILE_HEIGHT - gameThread.getGameCamera().getyOffset()));
-				}
+				getTile(row, col).render(g2d, (int) (row * Tile.TILE_WIDTH - gameThread.getGameCamera().getxOffset()),
+						(int) (col * Tile.TILE_HEIGHT - gameThread.getGameCamera().getyOffset()), randomedTiles[row][col]);
 			}
 		}
-
+		
 		entityManager.render(g2d);
-	}
-
-	public int[][] getRawTile() {
-		return tiles;
 	}
 
 	public Tile getTile(int row, int col) {
 		// return spaceTiles if out of world
-		if (row < 0 || col < 0 || row >= width || col >= height) {
+		if(row < 0 || col < 0 || row >= width || col >= height) {
 			return Tile.spaceTiles;
 		}
-
+		
 		Tile t = Tile.tiles[tiles[row][col]]; // get Tile from tile id
 		if (t == null) {
 			return Tile.dirtTiles; // return default tile
 		}
-		return t;
-	}
-
-	public CliffTile getCliffTile(int row, int col) {
-		// return spaceTiles if out of world
-
-		CliffTile t = CliffTile.cliffTiles[tiles[row][col] - 256]; // get Tile from tile id
 		return t;
 	}
 
@@ -105,25 +77,19 @@ public class World {
 		for (int col = 0; col < height; col++) {
 			for (int row = 0; row < width; row++) {
 				tiles[row][col] = Utils.parseInt(tmp[row + col * width + 4]); // turn 1D array to 2D
-				if (tiles[row][col] < 256) {
-					randomedTiles[row][col] = (int) (Math.random() * Tile.tiles[tiles[row][col]].getTileVariation());
-				} else if (tiles[row][col] < 277) {
-					randomedTiles[row][col] = (int) (Math.random() * Tile.dirtTiles.getTileVariation());
-				} else {
-					randomedTiles[row][col] = -1;
-				}
+				randomedTiles[row][col] = (int) (Math.random() * Tile.tiles[tiles[row][col]].getTileVariation());
 			}
 		}
 	}
-
+	
 	public int getWidth() {
 		return width;
 	}
-
+	
 	public int getHeight() {
 		return height;
 	}
-
+	
 	public EntityManager getEntityManager() {
 		return entityManager;
 	}
