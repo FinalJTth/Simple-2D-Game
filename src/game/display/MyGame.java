043afbfd14681b743cefdaf7572eb9783460540b
Teeradham <5batman400@gmail.com>
2019-05-14 12:45:18 +0700

    Remove Screen from MyGame

--- src/game/display/MyGame.java
+++ src/game/display/MyGame.java
@@ -4,11 +4,8 @@ import game.engine.Game;
 
 public class MyGame {
 
-	private Game game;
-
 	public MyGame() {
-		this.game = new Game(800, 600, "My Game");
-		
+		new Game(800, 600, "My Game");
 	}
 
 	public static void main(String[] args) {
