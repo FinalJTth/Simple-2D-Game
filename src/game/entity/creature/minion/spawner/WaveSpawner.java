bebe852f70e916ce564dce57edd67e7be18728ae
Teeradham <5batman400@gmail.com>
2019-05-16 13:07:27 +0700

    Remove MinionSpawner, BigBlobSpawner
    WaveSpawner no longer extends from MinionSpawner

--- src/game/entity/creature/minion/spawner/WaveSpawner.java
+++ src/game/entity/creature/minion/spawner/WaveSpawner.java
@@ -6,22 +6,36 @@ import java.util.ArrayList;
 import game.engine.GameThread;
 import game.entity.creature.Creatures;
 import game.entity.creature.minion.BigBlob;
+import game.graphics.Animation;
+import game.graphics.Assets;
 
-public class WaveSpawner extends MinionSpawner {
+public class WaveSpawner {
 
 	private static final int DELAY = 1000;
+	
+	protected final Animation portal;
+	protected final GameThread gameThread;
+	
+	protected long timer, lastTime;
 	
 	private ArrayList<Creatures> minionToSpawn;
 	private int minionIndex;
 	private final float xPos1 = 200, yPos1 = 200, xPos2 = 1600, yPos2 = 200, xPos3 = 200, yPos3 = 1600, xPos4 = 1600, yPos4 = 1600;
 
 	public WaveSpawner(GameThread gameThread, ArrayList<Creatures> minionToSpawn) {
-		super(gameThread, 0, 0);
+		this.gameThread = gameThread;
+		
+		portal = new Animation(100, Assets.portal);
+		
 		this.minionToSpawn = minionToSpawn;
 		minionIndex = 0;
 	}
 
-	@Override
+	public void update() {
+		spawnMinion();
+		portal.timerCounter();
+	}
+	
 	public void render(Graphics2D g2d) {
 		g2d.drawImage(portal.getCurrentFrame(), (int) (xPos1 - gameThread.getGameCamera().getxOffset()),
 				(int) (yPos1 - gameThread.getGameCamera().getyOffset()), 200, 200, null);
@@ -33,7 +47,6 @@ public class WaveSpawner extends MinionSpawner {
 				(int) (yPos4 - gameThread.getGameCamera().getyOffset()), 200, 200, null);
 	}
 
-	@Override
 	public void spawnMinion() {
 		timer += System.currentTimeMillis() - lastTime;
 		lastTime = System.currentTimeMillis();
