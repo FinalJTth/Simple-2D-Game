90beeeed013af951d78d9b222f4b5ba2e44d36de
FinalJTth <FinalJTth_31@hotmail.com>
2019-05-16 16:36:37 +0700

    Change all MenuButton into field variable.

--- src/game/listener/MouseManager.java
+++ src/game/listener/MouseManager.java
@@ -9,6 +9,10 @@ public class MouseManager implements MouseListener, MouseMotionListener {
 	private int mouseX, mouseY;
 	private boolean leftPressed, rightPressed;
 
+	public MouseManager() {
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	public void mouseClicked(MouseEvent event) {
 
