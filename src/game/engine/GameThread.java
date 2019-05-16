dd33a37ad36c5b98ad43d1babf2d2396e2bb403c
Teeradham <5batman400@gmail.com>
2019-05-16 22:20:37 +0700

    Tweak little bit

--- src/game/engine/GameThread.java
+++ src/game/engine/GameThread.java
@@ -70,7 +70,7 @@ public class GameThread implements Runnable {
 			e.printStackTrace();
 		}
 
-		int fps = 120;
+		int fps = 60;
 		double timePerTick = ONE_BILLION / fps;
 		double delta = 0;
 		long currentTime;
@@ -186,7 +186,7 @@ public class GameThread implements Runnable {
 //		g2d.drawLine(0, window.getHeight() / 2, window.getWidth(),
 //				window.getHeight() / 2);
 
-		g2d.drawString(Integer.toString(currentFPS), 10, 10);
+//		g2d.drawString(Integer.toString(currentFPS), 10, 10);
 
 		// end drawing
 		bs.show();
