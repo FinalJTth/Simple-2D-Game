21cdb4a48f35752acf0093f7112c3d3db684386e
FinalJTth <FinalJTth_31@hotmail.com>
2019-05-16 16:55:54 +0700

    Rearrange code inside SoundPlayer.java

--- src/game/soundFX/SoundPlayer.java
+++ src/game/soundFX/SoundPlayer.java
@@ -1,48 +1,21 @@
 package game.soundFX;
 
 import java.io.File;
-import java.io.IOException;
 
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
-
-import game.engine.GameThread;
 import javafx.application.Platform;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaPlayer.Status;
 
 public class SoundPlayer {
-	
+
 	public static SoundPlayer small_explosion;
 	public static SoundPlayer pewpew;
 	public static SoundPlayer bgm, menuBgm;
-	
-	protected static boolean isSoundOFF = false;
-	
-	public static void initGameSound() {
-		small_explosion = new SoundPlayer("sound/smallBomb.mp3");
-		pewpew = new SoundPlayer("sound/pew.wav");
-		bgm = new SoundPlayer("sound/bgm.mp3");
-	}
-	
-	public static void initMenuSound() {
-		menuBgm = new SoundPlayer("sound/menubgm.mp3");
-	}
-	
-	public static void toggleSound() {
-		isSoundOFF = !isSoundOFF;
-		if (menuBgm.getMediaPlayer().getStatus() == Status.PLAYING) {
-			menuBgm.stopPlaying();
-		} else {
-			menuBgm.playNonStop();
-		}
-	}
 
 	protected MediaPlayer mediaPlayer;
+
+	protected static boolean isSoundOFF = false;
 
 	public SoundPlayer(String path) {
 
@@ -57,6 +30,25 @@ public class SoundPlayer {
 		});
 	}
 
+	public static void initGameSound() {
+		small_explosion = new SoundPlayer("sound/smallBomb.mp3");
+		pewpew = new SoundPlayer("sound/pew.wav");
+		bgm = new SoundPlayer("sound/bgm.mp3");
+	}
+
+	public static void initMenuSound() {
+		menuBgm = new SoundPlayer("sound/menubgm.mp3");
+	}
+
+	public static void toggleSound() {
+		isSoundOFF = !isSoundOFF;
+		if (menuBgm.getMediaPlayer().getStatus() == Status.PLAYING) {
+			menuBgm.stopPlaying();
+		} else {
+			menuBgm.playNonStop();
+		}
+	}
+
 	public void playSound() {
 		if (!isSoundOFF) {
 			mediaPlayer.play();
@@ -64,23 +56,23 @@ public class SoundPlayer {
 		}
 
 	}
-	
+
 	public void setVolume(double volume) {
 		mediaPlayer.setVolume(volume);
 	}
-	
+
 	public void playNonStop() {
 		if (!isSoundOFF) {
 			mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
 			mediaPlayer.play();
 		}
-		
+
 	}
-	
+
 	public void stopPlaying() {
 		mediaPlayer.stop();
 	}
-	
+
 	public MediaPlayer getMediaPlayer() {
 		return mediaPlayer;
 	}
